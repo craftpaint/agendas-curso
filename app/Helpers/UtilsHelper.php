485791b6c13820434d1a2d8ffa279c7aacabaeaf
Mechaz123 <danielalejandrorojasgonzalez@gmail.com>
2025-08-13 10:56:59 -0500

    fix: Se valida para un solo agente callcenter.

--- app/Helpers/UtilsHelper.php
+++ app/Helpers/UtilsHelper.php
@@ -23,62 +23,64 @@ class UtilsHelper {
         $sede = DB::table('tb_sede')->where('id_sede', $cita->id_sede)->first();
         $datosUsuario = $this->sendPulseWhatsapp->searchContactByPhone($cliente->telefono_cliente);
 
-        //Corrección de estructura de datos
-        $fechaFormateada = date('Y/m/d', strtotime($cita->reserva_cita));
-        $partes = explode('-', $cita->rango_horario);
-        $horaFormateada = trim($partes[0] ?? '');
+        if ($agente->email == "mariafernandagallo7@gmail.com"){
+            //Corrección de estructura de datos
+            $fechaFormateada = date('Y/m/d', strtotime($cita->reserva_cita));
+            $partes = explode('-', $cita->rango_horario);
+            $horaFormateada = trim($partes[0] ?? '');
 
-        $data = [
-            'bot_id' => env('SENDPULSE_WHATSAPP_BOT_ID'),
-            'phone' => $cliente->telefono_cliente,
-            'template' => [
-                'name' => 'utilidad_confirmacion_cita',
-                'components' => [
-                    [
-                        'type' => 'body',
-                        'parameters' => [
-                            [
-                                'type' => 'text',
-                                'text' => $cliente->nombre_cliente
-                            ],
-                            [
-                                'type' => 'text',
-                                'text' => $cliente->apellido_cliente
-                            ],
-                            [
-                                'type' => 'text',
-                                'text' => $agente->name
-                            ],
-                            [
-                                'type' => 'text',
-                                'text' => $fechaFormateada
-                            ],
-                            [
-                                'type' => 'text',
-                                'text' => $horaFormateada
-                            ],
-                            [
-                                'type' => 'text',
-                                'text' => $sede->nombre_sede
-                            ],
-                            [
-                                'type' => 'text',
-                                'text' => $sede->direccion_sede
+            $data = [
+                'bot_id' => env('SENDPULSE_WHATSAPP_BOT_ID'),
+                'phone' => $cliente->telefono_cliente,
+                'template' => [
+                    'name' => 'utilidad_confirmacion_cita',
+                    'components' => [
+                        [
+                            'type' => 'body',
+                            'parameters' => [
+                                [
+                                    'type' => 'text',
+                                    'text' => $cliente->nombre_cliente
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'text' => $cliente->apellido_cliente
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'text' => $agente->name
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'text' => $fechaFormateada
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'text' => $horaFormateada
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'text' => $sede->nombre_sede
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'text' => $sede->direccion_sede
+                                ]
                             ]
                         ]
+                    ],
+                    'language' => [
+                        'policy' => 'deterministic',
+                        'code' => 'es'
                     ]
-                ],
-                'language' => [
-                    'policy' => 'deterministic',
-                    'code' => 'es'
                 ]
-            ]
-        ];
-        if($datosUsuario){
-            $this->sendPulseWhatsapp->sendWhatsappTemplateByPhone($data);
-        } else {
-            $this->sendPulseWhatsapp->createContactWhatsapp($cliente->telefono_cliente, $cliente->nombre_cliente, [], []);
-            $this->sendPulseWhatsapp->sendWhatsappTemplateByPhone($data);
+            ];
+            if($datosUsuario){
+                $this->sendPulseWhatsapp->sendWhatsappTemplateByPhone($data);
+            } else {
+                $this->sendPulseWhatsapp->createContactWhatsapp($cliente->telefono_cliente, $cliente->nombre_cliente, [], []);
+                $this->sendPulseWhatsapp->sendWhatsappTemplateByPhone($data);
+            }
         }
     }
 }
