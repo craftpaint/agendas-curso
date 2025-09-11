9f8500a0a75155422617de107e638718aecce0b8
Miguel Angel Manrique Hernandez <miguel070999@hotmail.com>
2025-09-11 11:14:26 -0500

    Revert "Release"

--- app/Http/Controllers/LoadController.php
+++ app/Http/Controllers/LoadController.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Crypt;
 use Carbon\Carbon;
 use App\Jobs\WhatsappJob;
-use App\Jobs\ScrapingSimitJob;
 
 class LoadController extends Controller
 {
@@ -288,7 +287,6 @@ class LoadController extends Controller
                             $email_cliente,
                             $nombre_cliente,
                             $nombre_cliente . " Confirmamos tu cita",
-                            $doc_cliente,
                             $templateVariables
                         );
 
@@ -305,20 +303,6 @@ class LoadController extends Controller
 
                     // Envía el mensaje de WhatsApp al cliente
                     WhatsappJob::dispatch($id_cita, $citas_agendadas)->onQueue('Whatsapp');
-
-                    // Se envía la cita para validar en el SIMIT
-                    $metodo_actual = DB::table('tb_config')
-                        ->where('config_key', 'method_scraping')
-                        ->value('config_value');
-                    
-                    switch ($metodo_actual) {
-                        case 1:
-                            ScrapingSimitJob::dispatch($id_cita, $doc_cliente)->onQueue('Scraping');
-                            break;
-                        case 2:
-                            Log::info("Se enviaría al Agente ChatGPT para realizar el Scraping.");
-                            break;
-                    }
 
                     try {
                         $saveliquidador = DB::table('tb_liquidador')->insert([
--- config/filesystems.php
+++ config/filesystems.php
@@ -35,6 +35,7 @@ return [
             'root' => storage_path('app'),
             'throw' => false,
         ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
@@ -56,13 +57,7 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
-        'eventos' => [
-            'driver' => 'local',
-            'root' => base_path('public_html/eventos'),
-            'url' => env('APP_URL') . '/public_html/eventos',
-            'visibility' => 'public',
-            'throw' => false,
-        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
